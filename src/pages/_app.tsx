c0a4024f3d805fdd3e9d39b09fc0908ce4f06c94
Joseph-Cecil <josephgodwins015@gmail.com>
2023-08-25 17:12:57 +0100

    first session

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,35 @@ import { SessionProvider } from "next-auth/react";
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
+import {useColorScheme} from "@mantine/hooks";
+import { useState } from "react";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
+  const preferredColorScheme = useColorScheme();
+  console.log(preferredColorScheme)
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme);
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      theme={{
+        /** Put your mantine theme override here */
+        colorScheme
+      }}> 
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+  </MantineProvider>
+  </ColorSchemeProvider>
   );
 };
 
